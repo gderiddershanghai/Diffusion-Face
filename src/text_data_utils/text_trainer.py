b707f4ffe88d39605e396e7d8b819396fef2b411
Ginger de Ridder <ginger_de_ridder@hotmail.com>
2025-02-13 09:35:54 +0800

    Quick commit

--- src/text_data_utils/text_trainer.py
+++ src/text_data_utils/text_trainer.py
@@ -8,17 +8,19 @@ import joblib
 
 
 
-# if __name__ == "__main__":
+if __name__ == "__main__":
     print('testing imports')
     fp = '/home/ginger/code/gderiddershanghai/Diffusion-Face/data/text_data/cleaned_DFDB_Face_metadata.csv'
     df = pd.read_csv(fp)
-
+    y_train = df['nsfw'].values
+    print(y_train[:10])
     # import Feature Extractor
     extractor = BertFeatureExtractor()
     # Get Mean Text Embeddings
     train_embeddings = extractor.transform(df, text_column='prompt')
 
-    y_train = df['nsfw'].values
+    # y_train = df['nsfw'].values
+    # print(y_train[:10])
     X_train, X_val, y_train, y_val = train_test_split(train_embeddings, 
                                                       y_train, 
                                                       test_size=0.15, 
@@ -26,14 +28,15 @@ import joblib
 
     # Train SVM - safe weights
     C = 1
+    thres = 0.85
+    y_val =  np.where(y_val > thres, 1, 0)
+    y_train = np.where(y_train > thres, 1, 0)
+    
     save_fp = f'/home/ginger/code/gderiddershanghai/Diffusion-Face/weights/nsfw_weights_{C}.pkl'
     svm_model = SVC(kernel='rbf', C=C, gamma='scale', random_state=42, probability=True)
     svm_model.fit(X_train, y_train)
     y_val_pred = svm_model.predict_proba(X_val)
 
-    thres = 0.85
-    y_val_pred = np.where(y_val_pred[:, 1] > thres, 1, 0)
-    y_val = np.where(y_val[:, 1] > thres, 1, 0)
 
     val_accuracy = accuracy_score(y_val, y_val_pred)
     print("Val Acc: ", val_accuracy)
@@ -44,33 +47,38 @@ import joblib
 
     #####################################################
     C = 50
+    thres = 0.85
+    y_val =  np.where(y_val > thres, 1, 0)
+    y_train = np.where(y_train > thres, 1, 0)
+    
     save_fp = f'/home/ginger/code/gderiddershanghai/Diffusion-Face/weights/nsfw_weights_{C}.pkl'
     svm_model = SVC(kernel='rbf', C=C, gamma='scale', random_state=42, probability=True)
     svm_model.fit(X_train, y_train)
+    
     y_val_pred = svm_model.predict_proba(X_val)
-
-    thres = 0.85
     y_val_pred = np.where(y_val_pred[:, 1] > thres, 1, 0)
-    y_val = np.where(y_val[:, 1] > thres, 1, 0)
+
     
     val_accuracy = accuracy_score(y_val, y_val_pred)
     print("Val Acc: ", val_accuracy)
     print(classification_report(y_val, y_val_pred))
     
     # save weights
-    joblib.dump(svm_model, 'svm_model_weights.pkl')
+    joblib.dump(svm_model, save_fp)
     print(f"Model weights saved to 'svm_model_weights_c{C}.pkl'")
     
     
     C = 95
+    thres = 0.85
+    y_val =  np.where(y_val > thres, 1, 0)
+    y_train = np.where(y_train > thres, 1, 0)
     save_fp = f'/home/ginger/code/gderiddershanghai/Diffusion-Face/weights/nsfw_weights_{C}.pkl'
     svm_model = SVC(kernel='rbf', C=C, gamma='scale', random_state=42, probability=True)
     svm_model.fit(X_train, y_train)
     y_val_pred = svm_model.predict_proba(X_val)
 
-    thres = 0.85
     y_val_pred = np.where(y_val_pred[:, 1] > thres, 1, 0)
-    y_val = np.where(y_val[:, 1] > thres, 1, 0)
+
     val_accuracy = accuracy_score(y_val, y_val_pred)
     print("Val Acc: ", val_accuracy)
     print(classification_report(y_val, y_val_pred))
